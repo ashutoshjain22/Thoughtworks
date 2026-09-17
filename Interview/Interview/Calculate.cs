1aca8ede7650b1143cebff2d55f3e03849cdde12
ashutoshjain22@gmail.com <ashutoshjain22@gmail.com>
2026-09-17 10:35:07 +1000

    Adding Xunit reference from nuget

---
+++ Interview/Interview/Calculate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UnitTestCaseProject
+{
+    internal class Calculate
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Interview/Interview/CalculatorTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UnitTestCaseProject
+{
+   
+    public class CalculatorTest
+    {
+        [Fact]
+        public void TestAdd()
+        {
+            Calculate calculator = new Calculate();
+            int result = calculator.Add(2, 3);
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public void TestSubtract()
+        {
+            Calculate calculator = new Calculate();
+            int result = calculator.Subtract(5, 3);
+            Assert.Equal(2, result);                    
+        }
+        [Fact]  
+        public void TestMultiplication()
+        {
+            Calculate calculator = new Calculate();
+            int result = calculator.Multiplication(2, 3);
+            Assert.Equal(6, result);
+
+        }
+        [Fact]
+        public void TestDivide()
+        {
+            Calculate calculator = new Calculate();
+            int result = calculator.Divide(6, 3);
+            Assert.Equal(2, result);
+        }
+    }
+}
